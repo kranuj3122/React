f6708106d8cb978d76bf3c229b21776ac5a636e3
kranuj3122 <kumaranuj22799@gmail.com>
2021-01-29 16:20:41 +0530

    hello

---
+++ src/components/App.js
@@ -0,0 +1,16 @@
+import React, {Component, useState} from "react";
+import '../styles/App.css';
+import Description from './Description';
+import Name from './Name';
+
+const App = () => {
+  return (
+    <div id="main">
+      <Description />
+      <Name />
+    </div>
+  )
+}
+
+
+export default App;
---
+++ src/components/Name.js
@@ -0,0 +1,8 @@
+import React from "react";
+export default class Name extends React.Component {
+    render() {
+        return (
+        <div><h4>I am beginner</h4></div>
+        );
+    }
+}
